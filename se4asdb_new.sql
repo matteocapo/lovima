f09671943497f8eef0b62fa6637432b04493cc95
jerryildrago <matteo.capodicasa@gmail.com>
2018-01-23 15:13:19 +0100

    aggiunta query animals e modifica db
    Se lorenzo non ha modificato il db, copiate questo dump perché ho fatto
    delle modifiche sulla tabella degli animali

---
+++ se4asdb_new.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Creato il: Gen 23, 2018 alle 15:10
+-- Versione del server: 5.5.42
+-- Versione PHP: 5.6.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `se4asdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `active_boxes`
+--
+
+CREATE TABLE `active_boxes` (
+  `id` int(11) NOT NULL,
+  `id_box` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `animals`
+--
+
+CREATE TABLE `animals` (
+  `id` int(11) NOT NULL,
+  `id_animal` varchar(11) CHARACTER SET utf8 NOT NULL DEFAULT '0',
+  `type` varchar(30) CHARACTER SET utf8 DEFAULT '0',
+  `species` varchar(30) CHARACTER SET utf8 DEFAULT '0',
+  `sex` varchar(5) CHARACTER SET utf8 DEFAULT '0',
+  `maxNPost` int(11) DEFAULT '0',
+  `minTemp` int(11) DEFAULT '0',
+  `food` int(11) DEFAULT '0',
+  `foodDoses` int(11) DEFAULT '0',
+  `maxTemp` float DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `boxes`
+--
+
+CREATE TABLE `boxes` (
+  `id` int(11) NOT NULL,
+  `id_box` varchar(11) DEFAULT '0',
+  `idAnimals` varchar(11) DEFAULT '0',
+  `dateTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `animalsN` int(11) DEFAULT '0',
+  `temp` float DEFAULT '0',
+  `foodQnt` double DEFAULT '0',
+  `waterQnt` double DEFAULT '0',
+  `light` int(11) DEFAULT '0',
+  `humidity` float DEFAULT '0',
+  `alarm` tinyint(1) DEFAULT '0',
+  `display` tinyint(1) DEFAULT '0',
+  `windler` tinyint(1) DEFAULT '0',
+  `type` varchar(30) DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `boxes`
+--
+
+INSERT INTO `boxes` (`id`, `id_box`, `idAnimals`, `dateTime`, `animalsN`, `temp`, `foodQnt`, `waterQnt`, `light`, `humidity`, `alarm`, `display`, `windler`, `type`) VALUES
+(1, '1', '0', NULL, 0, 25, 0, 0, 0, 0, 0, 0, 0, 'T'),
+(2, '2', '0', NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'T'),
+(3, '1', '0', NULL, 0, 0, 0, 0, 233, 0, 0, 0, 0, '0'),
+(4, '1', '0', '2018-01-19 17:25:38', 0, 0, 0, 0, 233, 0, 0, 0, 0, '0'),
+(5, '3', '0', '2018-01-20 14:50:08', 0, 0, 0, 0, 0, 0, 0, 0, 0, '0'),
+(6, '3', '0', '2018-01-20 14:51:01', 0, 0, 0, 0, 0, 0, 1, 0, 0, '0'),
+(7, '1', '0', '2018-01-20 14:58:39', 0, 0, 0, 0, 0, 0, 0, 0, 0, '0'),
+(8, '25', '123', '2018-01-20 15:26:16', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'T'),
+(11, '25', '0', '2018-01-20 15:28:06', 1, 300, 1, 1, 111, 11112, 0, 0, 0, '0'),
+(12, '25', '0', '2018-01-20 16:24:39', 300, 1, 1, 1, 111, 11112, 1, 0, 0, '0'),
+(13, '25', '0', '2018-01-20 16:25:55', 300, 1, 1, 1, 111, 11112, 1, 0, 0, '0'),
+(14, '25', '0', '2018-01-20 16:28:17', 300, 1, 1, 1, 111, 11112, 1, 1, 0, '0'),
+(15, '25', '0', '2018-01-20 16:39:01', 300, 1, 50, 1, 111, 11112, 1, 1, 0, '0'),
+(16, '25', '0', '2018-01-20 16:42:42', 300, 1, 50, 1, 111, 11112, 1, 1, 0, '0'),
+(17, '25', '0', '2018-01-20 16:42:42', 300, 1, 50, 1, 111, 23.6, 1, 1, 0, '0');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `active_boxes`
+--
+ALTER TABLE `active_boxes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `animals`
+--
+ALTER TABLE `animals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `boxes`
+--
+ALTER TABLE `boxes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_box` (`id_box`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `active_boxes`
+--
+ALTER TABLE `active_boxes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `animals`
+--
+ALTER TABLE `animals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `boxes`
+--
+ALTER TABLE `boxes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
---
+++ src/main/java/it/univaq/disim/se4s/dbquery/DbAnimalsInterface.java
@@ -0,0 +1,230 @@
+package it.univaq.disim.se4s.dbquery;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.Map;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+
+public class DbAnimalsInterface {
+  
+  public static Connection connector() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			} 
+		catch (ClassNotFoundException e) {
+			System.out.println("Non sono stati caricari i driver");
+			e.printStackTrace();
+			}
+		
+		System.out.println("Driver registrati");
+		Connection connection = null;
+
+		try {
+			connection = DriverManager
+			.getConnection("jdbc:mysql://localhost:3306/se4asdb","root", "root");
+			} 
+		catch (SQLException e) {
+			try {
+				connection = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/se4asdb","root", "");
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Connessione non riuscita");				
+			}
+		}
+
+		if (connection != null) {
+			System.out.println("Connessione riuscita");
+			return connection;
+		} else {
+			System.out.println("Connessione fallita");
+			return null;
+		}
+	}
+  
+  public static void querygenerica() {
+	  Connection connection = connector();
+	  try {
+		  Statement stmt = connection.createStatement();
+		  try {
+			  stmt.executeUpdate( "INSERT INTO `se4asdb`.`boxs` (`id`, `idMqtt`, `idAnimals`, `dateTime`, `animalsN`, `Temp`, `foodQnt`, `waterQnt`, `light`, `humidity`) VALUES ('2', '2', '2', '2018-01-02', '2', '24.5', '30', '40', '10', '15.4');" ); 
+			  System.out.println("query riuscita");
+			  stmt.close(); 	  
+			  connection.close();
+
+		  }
+		  catch (Exception e) {
+			  System.out.println(e);
+			  System.out.println("Errore query");
+		}
+	  }
+	  catch (SQLException e) {
+			  System.out.println("Errore Statment");
+			  e.printStackTrace();
+			  }	
+	  }
+  
+    
+  public static int boolToInt(boolean value) {
+	  if(value)
+		  return 1;
+	  else
+		  return 0;
+  }
+  
+  public static void setAll(String id, String type, String species, String sex, Integer maxNPos, Float minTemp, Float maxTemp, Integer food, String foodDoses) throws SQLException {
+	  
+		Connection connection = connector();
+
+		  try {
+			  Statement stmt = connection.createStatement();
+			  try {
+				  
+				  
+				  stmt.executeUpdate("INSERT INTO `se4asdb`.`animals`(`id_animal`, `type`, `species`, `sex`, `maxNPost`, `minTemp`, `food`, `foodDoses`, `maxTemp`) "
+				  + "values('"+id+"', '"+type+"', '"+species+"', '"+sex+"', '"+maxNPos+"', '"+minTemp+"', '"+maxTemp+"', '"+food+"', '"+foodDoses+"',)");
+				  stmt.close(); 	  
+				  connection.close();
+				  System.out.println("query riuscita");
+			  }
+			  catch (Exception e) {
+				  System.out.println(e);
+				  System.out.println("Errore query");
+			}
+		  }
+		  catch (SQLException e) {
+				  System.out.println("Errore Statment");
+				  e.printStackTrace();
+				  }	
+		  connection.close(); 
+	}
+  
+  public static Map<String, String> getAll(String id) throws SQLException {
+		 
+	  Map<String, String> diz = new HashMap<String, String>();
+	  
+	  Connection connection = connector();
+
+	  String query = "SELECT * FROM `animals` WHERE `id_animal`= '"+id+"' ORDER BY id DESC LIMIT 1";
+
+	  
+	  try {
+		  Statement stmt = connection.createStatement();
+		  try {
+			  
+			  ResultSet rs = stmt.executeQuery(query);
+			  while (rs.next()) {
+				  
+				  diz.put("idAnimal", rs.getString("idAnimal"));
+				  diz.put("type", rs.getString("type"));
+				  diz.put("species", rs.getString("species"));
+				  diz.put("sex", rs.getString("sex"));
+				  diz.put("maxNPost", rs.getString("maxNPost"));
+				  diz.put("minTemp", rs.getString("minTemp"));
+				  diz.put("food", rs.getString("food"));
+				  diz.put("foodDoses", rs.getString("foodDoses"));
+				  diz.put("maxTemp", rs.getString("maxTemp"));
+			  }
+			  System.out.println("query riuscita");
+			  
+			  stmt.close(); 	  
+			  connection.close();
+			  return diz;
+			  
+		  }
+		  catch (Exception e) {
+			  e.getStackTrace();
+			  System.out.println(e);
+			  System.out.println("Errore query");
+		}
+	  }
+	  catch (SQLException e) {
+			  System.out.println("Errore Statment");
+			  e.printStackTrace();
+			  }	
+	  connection.close();
+	  return diz;
+  }
+  
+  public static Map<String, Float> tempRange(String id) throws SQLException {
+	  
+	  Map<String, Float> diz = new HashMap<String, Float>();
+
+	  Connection connection = connector();
+
+	  String query = "SELECT * FROM `animals` WHERE `id_animal`= '"+id+"' ORDER BY id DESC LIMIT 1";
+	  
+	  try {
+		  Statement stmt = connection.createStatement();
+		  try {
+			  
+			  ResultSet rs = stmt.executeQuery(query);
+			  while (rs.next()) {
+				  diz.put("minTemp", rs.getFloat("minTemp"));
+				  diz.put("maxTemp", rs.getFloat("maxTemp"));
+			  }
+
+			  System.out.println("query riuscita");
+			  
+			  stmt.close(); 	  connection.close();
+			  return diz;
+			  
+		  }
+		  catch (Exception e) {
+			  e.getStackTrace();
+			  System.out.println(e);
+			  System.out.println("Errore query");
+		}
+	  }
+	  catch (SQLException e) {
+			  System.out.println("Errore Statment");
+			  e.printStackTrace();
+			  }	
+	  connection.close();
+	  return diz;
+  }
+  
+  public static int getFoodDoses(String id) throws SQLException {
+	  int value = 0;
+	  Connection connection = connector();
+	  
+	  String query = "SELECT * FROM `animals` WHERE `id_animal`= '"+id+"' ORDER BY id DESC LIMIT 1";
+
+	  try {
+		  Statement stmt = connection.createStatement();
+		  try {
+			  
+			  ResultSet rs = stmt.executeQuery(query);
+			  while (rs.next()) {
+				 value = rs.getInt("foodDoses");
+			  }
+
+			  System.out.println("query riuscita");
+			  
+			  stmt.close(); 	  
+			  connection.close();
+			  return value;
+			  
+		  }
+		  catch (Exception e) {
+			  e.getStackTrace();
+			  System.out.println(e);
+			  System.out.println("Errore query");
+		}
+	  }
+	  catch (SQLException e) {
+			  System.out.println("Errore Statment");
+			  e.printStackTrace();
+			  }	
+	  connection.close();
+	  return value;
+	
+  }
+}
+
+  
