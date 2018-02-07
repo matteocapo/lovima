753d281a0aca6d073f04e4dd062a2ccfcf83eba9
jerryildrago <matteo.capodicasa@gmail.com>
2018-02-07 19:00:31 +0100

    cdv file
    scarica il csv che contiene le informazioni del box selezionato

--- src/main/java/it/univaq/disim/se4s/dbquery/DbInterface.java
+++ src/main/java/it/univaq/disim/se4s/dbquery/DbInterface.java
@@ -2,9 +2,12 @@ package it.univaq.disim.se4s.dbquery;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.List;
 import java.util.Map;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -925,6 +928,55 @@ public class DbInterface {
 			  }	
 	  connection.close();
 	  return idAnimal;
+  }
+  
+  public static void getCSV(String id) throws SQLException {
+	  Connection connection = connector();
+ 	  String idAnimal = null;
+	  String query = "SELECT * FROM `boxes` WHERE `id_box`= '"+id+"'";
+	  
+	  try {
+		  Statement stmt = connection.createStatement();
+		  try {
+			  
+			  ResultSet rs = stmt.executeQuery(query);
+			  convertToCsv(rs, id);
+			  
+			  stmt.close(); 	  
+			  connection.close();
+			  
+		  }
+		  catch (Exception e) {
+			  e.getStackTrace();
+			  System.out.println(e);
+			  System.out.println("Errore query");
+		}
+	  }
+	  catch (SQLException e) {
+			  System.out.println("Errore Statment");
+			  e.printStackTrace();
+			  }	
+	  connection.close();
+  }
+  
+  public static void convertToCsv(ResultSet rs, String id) throws SQLException, FileNotFoundException {
+      
+	  PrintWriter csvWriter = new PrintWriter(new File("data_of_box_"+id+".csv")) ;
+      ResultSetMetaData meta = rs.getMetaData() ; 
+      int numberOfColumns = meta.getColumnCount() ; 
+      String dataHeaders = "\"" + meta.getColumnName(1) + "\"" ; 
+      for (int i = 2 ; i < numberOfColumns + 1 ; i ++ ) { 
+              dataHeaders += ",\"" + meta.getColumnName(i).replaceAll("\"","\\\"") + "\"" ;
+      }
+      csvWriter.println(dataHeaders) ;
+      while (rs.next()) {
+          String row = "\"" + rs.getString(1).replaceAll("\"","\\\"") + "\""  ; 
+          for (int i = 2 ; i < numberOfColumns + 1 ; i ++ ) {
+              row += ",\"" + rs.getString(i).replaceAll("\"","\\\"") + "\"" ;
+          }
+      csvWriter.println(row) ;
+      }
+      csvWriter.close();
   }
 	
 }
--- src/main/java/it/univaq/disim/se4s/mqttfunction/App.java
+++ src/main/java/it/univaq/disim/se4s/mqttfunction/App.java
@@ -12,18 +12,7 @@ import it.univaq.disim.se4s.dbquery.DbInterface;
 public class App {
 	
 	public static void main(String[] args) throws MqttException, SQLException {
-		//System.out.println("inizio");
-		//DbInterface.setOnlineBoxes("1", "T");
-		//DbInterface.setOnlineBoxes("2", "T");
-		//readFunction.readThemperature("1");
-		//readFunction.readThemperature("2");
-		//DbInterface.setLight("1", 237);
-		//System.err.println(DbInterface.getLight("1"));
-		//System.out.println("fine");
-		//whoIsFunction.whoIs();
-		//readFunction.readThemperature("25");
-		//DbInterface.setFoodQnt("25", "50");
-		//DbInterface.setHumidity("25", (float) 23.6);
+		DbInterface.getCSV("25");
 
 	}
 
